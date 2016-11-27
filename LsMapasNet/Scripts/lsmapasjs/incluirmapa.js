e9301019905a7ef4deba5228ecb76f0da2a29906
nielson266 <nielson266@gmail.com>
2016-11-27 09:04:20 -0300

    Ainda sem concluir a exclusão

--- LsMapasNet/Scripts/lsmapasjs/incluirmapa.js
+++ LsMapasNet/Scripts/lsmapasjs/incluirmapa.js
@@ -24,17 +24,13 @@ $("#btnIncluirMapaSurdo").click(function () {
         alert('Selecione um surdo!');
     }
     else {
-        console.log()
         var idmapa = $("#IdMapa").val();
-        console.log(idmapa);
         var idselectsurdo = $("#surdoNome").val();
-        console.log(idselectsurdo);
         $.post("/mapa/IncluirSurdoMapaJson", { IdMapa: idmapa, SelectIdSurdo: idselectsurdo }, function (data) {
             $.get("/mapa/_listasurdomapa", { id: idmapa }, function (data) {
                 $("#dvListaSurdoMapa").html(data);
             });
             $.get("/mapa/dropdownlistsurdo", function (dados) {
-                console.log(dados);
                 var options = {
                     data: dados,
                     list: {
@@ -51,16 +47,22 @@ $("#btnIncluirMapaSurdo").click(function () {
         });
     }
 });
-$(".btnExcluirSurdoMapa").click(function () {
-    var idmapa = $("#IdMapa").val();
-    var btnidsurdo = $(".btnExcluirSurdoMapa");
-    var idsurdo = btnidsurdo.data("id");
-    
-
-    $.post("/mapa/ExcluirSurdoMapa", { idsurdo: idsurdo }, function (data) {
-        $.get("/mapa/_listasurdomapa", { id: idmapa }, function (data) {
-            $("#dvListaSurdoMapa").html(data);
-        });
-    });
-    
+$(".btnExcluirSurdoMapa a").on("click", function (event) {
+    alert('Click on')
+    console.log($(this).text() + ' on' );
 });
+
+//$(".btnExcluirSurdoMapa a").click(function () {
+//    var idmapa = $("#IdMapa").val();
+//    var btnidsurdo = $(".btnExcluirSurdoMapa");
+//    var idsurdo = btnidsurdo.data("id");
+    
+//    console.log("Click");
+
+//    $.post("/mapa/ExcluirSurdoMapa", { idsurdo: idsurdo }, function (data) {
+//        $.get("/mapa/_listasurdomapa", { id: idmapa }, function (data) {
+//            $("#dvListaSurdoMapa").html(data);
+//        });
+//    });
+    
+//});
