f4422e260e13acf3533e00be0936e7f083cb8cee
nielson266 <nielson266@gmail.com>
2016-12-01 20:33:52 -0200

    01122016N

---
+++ LsMapasNet/obj/Release/Package/PackageTmp/Scripts/materializejs/js/iniciamaterialize.js
@@ -0,0 +1,15 @@
+﻿$(".button-collapse").sideNav();
+$('.fixed-action-btn').openFAB();
+$('.fixed-action-btn').closeFAB();
+$('.fixed-action-btn.toolbar').openToolbar();
+$('.fixed-action-btn.toolbar').closeToolbar();
+
+$(".dropdown-button").dropdown();
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+});
+
+$('textarea').val('New Text');
+$('textarea').trigger('autoresize');
